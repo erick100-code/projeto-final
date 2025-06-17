f03abce65bc801730ed765188c5eb41cde39c422
Erick Eduardo Souza Araujo <ericksouza23135@gmail.com>
2025-06-17 19:31:43 -0300

    rolagem removida

--- src/javascript/modal.js
+++ src/javascript/modal.js
@@ -6,11 +6,14 @@ bot.addEventListener('click', () => {
     const modal = document.getElementById(att)
 
     modal.showModal()
+    document.body.style.overflow = 'hidden'
 })
 
 fechar.addEventListener('click', () => {
     const attf = fechar.getAttribute('data-modal')
     const modal = document.getElementById(attf) 
-
+    
     modal.close()
+    document.body.style.overflow = 'auto'
+
 })
