1230bda448fe122530b8d1df8fbb93a354c4f140
Erick Eduardo Souza Araujo <ericksouza23135@gmail.com>
2025-06-17 15:36:01 -0300

    coursel finalizado

---
+++ src/javascript/courser.js
@@ -0,0 +1,33 @@
+const car = document.querySelectorAll('.carbot')
+const images = document.querySelectorAll('.coursel-images')
+let atualmage = 0
+const maximage = images.length
+
+car.forEach(car => {
+    car.addEventListener('click', () => {
+        let left = car.classList.contains('botleft')
+    
+        if (left) {
+            atualmage -= 1
+        } else {
+            atualmage += 1
+        }
+
+        if (atualmage < 0) {
+            atualmage = maximage - 1
+        }
+
+        if (atualmage >= maximage) {
+            atualmage = 0
+        }
+        
+        images.forEach(image => image.classList.remove('acendida'))
+
+        images[atualmage].scrollIntoView({
+            inline: "center",
+            behavior: "smooth"
+        })
+
+        images[atualmage].classList.add('acendida')
+    })
+})
